ed236823f0b492731bccdf1caf39f0da567d1c87
EltonLai <EltonYM_Lai@moxa.com>
2025-01-02 14:57:40 +0800

    add test code

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/eltonlai2014/my-pkg/pkg/mylib"
+)
+
+func main() {
+	// test code for mylib
+	result := mylib.Add(2, 3)
+	fmt.Println(result) // 5
+}
